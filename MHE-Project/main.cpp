d8c645423e0e180319b4b4c7825f8a16b63a22cc
Rafał Ubermanowicz <rafal.ubermanowicz@o2.pl>
2022-11-05 01:55:00 +0100

    TASK 3

--- MHE-Project/main.cpp
+++ MHE-Project/main.cpp
@@ -1,19 +1,3 @@
-/**                             Nonogram Example
- *                  2 2 4 4                             2 2 4 4
- *            0 9 9 2 3 2 2 1 0                   0 9 9 2 3 2 2 1 0
- *          0| | | | | | | | | |                0| | | | | | | | | |   //0
- *          4| | | | | | | | | |                4| |█|█|█|█| | | | |   //1
- *          6| | | | | | | | | |                6| |█|█|█|█|█|█| | |   //2
- *        2 2| | | | | | | | | |              2 2| |█|█| | |█|█| | |   //3
- *        2 2| | | | | | | | | |              2 2| |█|█| | |█|█| | |   //4
- *          6| | | | | | | | | |                6| |█|█|█|█|█|█| | |   //5
- *          4| | | | | | | | | |                4| |█|█|█|█| | | | |   //6
- *        2 2| | | | | | | | | |              2 2| |█|█| |█|█| | | |   //7
- *        2 2| | | | | | | | | |              2 2| |█|█| | |█|█| | |   //8
- *        2 2| | | | | | | | | |              2 2| |█|█| | | |█|█| |   //9
- *          0| | | | | | | | | |                0| | | | | | | | | |   //10
- *
- */
 #include <iostream>
 #include <vector>
 #include <string>
@@ -30,16 +14,20 @@ struct nonogram_t {
     }
 
     int get(const int x, const int y) const {
+        if ((x < 0) || (x >= width) || (y < 0) || (y >= height)) throw std::invalid_argument("out of board");
         return board.at(y * width + x);
     }
 };
 
 
 std::ostream &operator<<(std::ostream &output, const nonogram_t &nonogram) {
-    output << "\t\t";
+    output << "\t";
     for (int w = 0; w < nonogram.width; w++) {
         for (int ccs = 0; ccs < nonogram.column_clues[w].size(); ccs++) {
-            output << nonogram.column_clues[w][ccs];
+            if (ccs < nonogram.column_clues[w].size() - 1)
+                output << nonogram.column_clues[w][ccs] << " ";
+            else
+                output << nonogram.column_clues[w][ccs];
         }
         output << "\t";
     }
@@ -47,81 +35,115 @@ std::ostream &operator<<(std::ostream &output, const nonogram_t &nonogram) {
 
     for (int h = 0; h < nonogram.height; h++) {
         for (int rcs = 0; rcs < nonogram.row_clues[h].size(); rcs++)
-            output << nonogram.row_clues[h][rcs];
-        output << "\t";
-        for (int w = 0; w < nonogram.width + 1; w++) {
-            auto &val = nonogram.board[h * nonogram.width + w];
-            output << ((val > 0) ? "███" :
-                       ((val < 1) ? "   " : "")) << "|";
+            if (rcs < (nonogram.row_clues[h].size() - 1))
+                output << nonogram.row_clues[h][rcs] << " ";
+            else
+                output << nonogram.row_clues[h][rcs] << "\t";
+        for (int w = 0; w < nonogram.width; w++) {
+            auto &value = nonogram.board[h * nonogram.width + w];
+            output << ((value == 1) ? "███" :
+                       ((value == 0) ? "   " : "")) << "|";
         }
         output << std::endl;
     }
     return output;
 }
 
-int count_inconsistent(nonogram_t &nonogram) {
-    bool first_seen = false;
-    //for ( int y = 0; y < nonogram.width){
-    //
-    //};
+/* The function is iterating first through the elements in rows and then through the elements in columns at the board
+ * to check if submitted answer == solution, the returned number describes the value of "Wrong rows + Wrong columns"
+ *
+ *  adds 1 if element != NONE and pushes the value to current set when it comes across NONE or row/column ends.
+ *  check if number set[x] == solution set[x], if not adds 1 to mistakes
+ */
+int count_inconsistent(const nonogram_t &nonogram) {
+    int mistakes = 0;
+    //Count wrong submitted Rows
+    for (int y = 0; y < nonogram.height; y++) {
+        std::vector<int> set = {};
+        int filled_squares = 0;
+        for (int x = 0; x < nonogram.width; x++) {
+            if (nonogram.get(x,y) == 1)
+                filled_squares++;
+            if ((nonogram.get(x,y) == 0 || x == nonogram.width - 1) && (filled_squares != 0)) {
+                set.push_back(filled_squares);
+                filled_squares = 0;
+            }
+        }
+        if (set.empty()) set.push_back(0);
+        if (set != nonogram.row_clues[y]) mistakes++;
+    }
+    //Count wrong submitted Rows
+
+    //Count wrong submitted Columns
+    for (int x = 0; x < nonogram.width; x++) {
+        std::vector<int> set = {};
+        int filled_squares = 0;
+        for (int y = x; y <= nonogram.width * nonogram.height; y += nonogram.width) {
+            if (nonogram.board[y]== 1)
+                filled_squares++;
+            if ((nonogram.board[y] == 0 || y == (nonogram.width * (nonogram.height - 1)) + x) && (filled_squares  != 0)){
+                set.push_back(filled_squares);
+                filled_squares  = 0;
+            }
+        }
+        if (set.empty()) set.push_back(0);
+        if (set != nonogram.column_clues[x]) mistakes++;
+    }
+    //Count wrong submitted Columns
+
+    return mistakes;
 }
 
 double evaluate(const nonogram_t &nonogram) {
+    return count_inconsistent(nonogram);
+}
 
+bool next_solution(nonogram_t &nonogram) {
+    int i = 0;
+    for (; i < nonogram.board.size(); i++) {
+        if (nonogram.board[i] == 0) {
+            nonogram.board[i] = 1;
+            break;
+        } else
+            nonogram.board[i] = 0;
+    }
+    return (i != nonogram.board.size());
 }
 
 nonogram_t nonogram = {
-        9,
-        11,
-        {{0}, {9}, {9}, {2, 2}, {2, 3}, {4, 2},
-         {4, 2}, {1}, {0}},
-        {{0}, {4}, {6}, {2, 2}, {2, 2}, {6}, {4},
-         {2, 2}, {2, 2}, {2, 2}, {0}},
-        {
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   //0
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //1
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //2
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //3
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //4
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //5
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //6
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //7
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //8
-                0, 0, 0, 0, 0, 0, 0, 0, 0, //9
-                0, 0, 0, 0, 0, 0, 0, 0, 0  //10
-        }
-
+        4,
+        4,
+        {{4}, {0}, {1, 1}, {4}},
+        {{1,2}, {1, 1}, {1, 1}, {1,2}},
+        {0, 0, 0, 0,
+         0, 0, 0, 0,
+         0, 0, 0, 0,
+         0, 0, 0, 0}
 };
 
-nonogram_t nonogram_solution = {
-        9,
-        11,
-        {{0}, {9}, {9}, {2, 2}, {2, 3}, {4, 2},
-         {4, 2}, {1}, {0}},
-        {{0}, {4}, {6}, {2, 2}, {2, 2}, {6},
-         {4}, {2, 2}, {2, 2}, {2, 2}, {0}},
-        {
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   //0
-                0, 1, 1, 1, 1, 0, 0, 0, 0, //1
-                0, 1, 1, 1, 1, 1, 1, 0, 0, //2S
-                0, 1, 1, 0, 0, 1, 1, 0, 0, //3
-                0, 1, 1, 0, 0, 1, 1, 0, 0, //4
-                0, 1, 1, 1, 1, 1, 1, 0, 0, //5
-                0, 1, 1, 1, 1, 0, 0, 0, 0, //6
-                0, 1, 1, 0, 1, 1, 0, 0, 0, //7
-                0, 1, 1, 0, 0, 1, 1, 0, 0, //8
-                0, 1, 1, 0, 0, 0, 1, 1, 0, //9
-                0, 0, 0, 0, 0, 0, 0, 0, 0  //10
+void Brute_Force() {
+    int n = 0;
+    while (next_solution(nonogram)) {
+        if ((n % 4096) == 0) {
+            std::cout << "Wrong board at ";
+            std::cout << n << " tries. Found " << evaluate(nonogram) << " mistakes in submitted Nonogram" << std::endl << nonogram << std::endl;
         }
 
+        if (evaluate(nonogram) == 0) {
+            std::cout << "Brute-Force found the right solution in " << n+1 << " tries." << std::endl;
+            std::cout << nonogram << std::endl;
+            break;
+        }
+        n++;
+    }
 };
 
 int main() {
-    std::cout << "Example of a NonogramPuzzle" << std::endl;
+    std::cout << "\n Nonogram to solve" << std::endl;
     std::cout << nonogram << std::endl;
 
-    std::cout << "Solved NonogramPuzzle" << std::endl;
-    std::cout << nonogram_solution;
+    std::cout << "Solving with Brute_Force Algorithm" << std::endl;
+    Brute_Force();
 
     return 0;
 }
